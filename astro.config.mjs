b8ec781ac2237f4f8ca7754fa8cd424fb0bb6e13
Wojciech Jelen <wjelen30@gmail.com>
2023-11-29 17:01:46 +0100

    chore: :wrench: vercel support & prettier config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [tailwind()],
+  site: "https://www.tyberos.com",
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
