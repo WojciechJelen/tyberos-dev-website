898a50c6bf67c9a1ee39a6897f6081760efa01f8
Wojciech Jelen <wjelen30@gmail.com>
2023-11-30 01:04:10 +0100

    feat: :sparkles: used content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.tyberos.com",
+  site: 'https://www.tyberos.com',
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
-});
+  output: 'hybrid',
+  adapter: vercel(),
+})
