85d670c51e397b7a1ce28789ab649c0158f4d836
Wojciech Jelen <wjelen30@gmail.com>
2023-12-04 13:05:00 +0100

    feat: :sparkles: Added mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/static'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
+import react from '@astrojs/react';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tyberos.com',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   output: 'static',
   adapter: vercel(),
   alias: {
     '@layouts': './src/layouts',
-    '@components': './src/components',
-  },
-})
+    '@components': './src/components'
+  }
+});
