15e2cb7a8699d829f500572da4215e05cbf9b538
Wojciech Jelen <wjelen30@gmail.com>
2023-12-06 12:23:40 +0100

    style: :sparkles:

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
 import react from '@astrojs/react';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
   adapter: vercel(),
   alias: {
     '@layouts': './src/layouts',
-    '@components': './src/components'
-  }
+    '@components': './src/components',
+  },
+  devOverlay: true,
 });
