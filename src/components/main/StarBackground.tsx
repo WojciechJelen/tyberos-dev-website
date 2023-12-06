b27ff8fa5553b3f9e921baf1028f27d2af6ace3c
Wojciech Jelen <wjelen30@gmail.com>
2023-12-06 12:28:52 +0100

    fixed import

--- src/components/main/StarBackground.tsx
+++ src/components/main/StarBackground.tsx
@@ -1,13 +1,14 @@
 import { useState, useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial } from '@react-three/drei';
-import { inSphere } from 'maath/random';
+// @ts-ignore
+import * as random from 'maath/random/dist/maath-random.cjs.prod';
 
 // @ts-ignore
 function StarBackground(props: any) {
   const ref: any = useRef();
   const [sphere] = useState(() =>
-    inSphere(new Float32Array(5000), { radius: 1.2 }),
+    random.inSphere(new Float32Array(5000), { radius: 1.2 }),
   );
 
   useFrame((state, delta) => {
